56459544553e15addb1dfefc4999acce111198c0
kost960 <pozitq3@gmail.com>
2024-03-09 21:40:07 +0700

    All tasks

---
+++ Task 8.py
@@ -0,0 +1,15 @@
+age_computer = 80
+name_human = input('Здравствуйте! Как Вас зовут?')
+print(f'Очень приятно,{name_human}! Меня зовут Марк.')
+age_human = int(input('Сколько Вам лет? '))
+if age_computer > age_human:
+    old = age_computer - age_human
+    print(f'Да,{name_human}, я старше Вас на {old} лет.')
+else:
+    old = age_human - age_computer
+    print(f'Да,{name_human}, Вы старше меня на {old} лет.')
+answer = input('Вам нравится программировать? ')
+if answer == 'да':
+    print(f'Превосходно! Уверен, у Вас получится написать множество полезных и хороших программ.')
+else:
+    print(f'Жаль. Я думал, Вы сможете написать интересную программу для меня.')
